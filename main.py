8476a715d21ac9db23c4a3f08db1abb712cb1389
Anon <anon@anon.anon>
2014-04-23 21:22:47 +0930

    Stable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, re, string, crypt, random
+import sys, re, string, crypt, random, sqlite3
 
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
@@ -11,6 +11,14 @@ def mktripcode(pw):
 	trip = crypt.crypt(pw, salt)[-10:]
 	return trip
 
+conn = sqlite3.connect('trips.db')
+curr = conn.cursor()
+curr.execute('CREATE TABLE IF NOT EXISTS Trips(pass TEXT, trip TEXT);')
+conn.commit()
+
 while True:
 	x = ''.join(random.choice(chars) for n in xrange(8))
-	print x, ":", mktripcode(str(x))
+	z = mktripcode(x)
+	print x,':',z
+	curr.execute('INSERT INTO Trips VALUES(\''+x+'\',\''+z+'\');')
+	conn.commit()
